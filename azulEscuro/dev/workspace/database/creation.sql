ae6c200b72740b631588fc7b084885273c911ee8
lucas003simoes@gmail.com <aksjd!#12@k>
2016-10-09 02:22:17 -0300

    Script inicial de criação do banco de dados

--- azulEscuro/dev/workspace/database/creation.sql
+++ azulEscuro/dev/workspace/database/creation.sql
@@ -1,46 +1,16 @@
-﻿CREATE TABLE Usuario
-(
-  nome VARCHAR(45)   NOT NULL,
-  login VARCHAR(25)  NOT NULL,
-  perfil VARCHAR(40)  NOT NULL,
-  passwordx VARCHAR(15) NOT NULL, 
-  PRIMARY KEY (login)
-);
-
-CREATE TABLE LocationPoint
-(
-   id serial NOT NULL,	
-   timestamp DATE NOT NULL,
-   latitude VARCHAR(25)  NOT NULL,
-   longitude VARCHAR(25)  NOT NULL,  
-   login VARCHAR(25) NOT NULL,
-
-   FOREIGN KEY(login) 
-	REFERENCES Usuario(login),
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE PROBLEMA_TIPO{
-   id serial NOT NULL,
-   desc VARCHAR(25) NOT NULL,
-   PRIMARY KEY(id)
-
-}
+﻿
 
 CREATE TABLE Problema(
    id serial NOT NULL,	
    timestamp DATE NOT NULL,
    latitude  VARCHAR(25)  NOT NULL,
    longitude VARCHAR(25)  NOT NULL,
-   
-
-
-
-)
-
-CREATE TABLE Viagem(
-   id serial NOT NULL,	
-   timeSrc DATE NOT NULL, 
-   
-
-)
+   tipo INT NOT NULL,
+   observacao VARCHAR(140) NOT NULL,
+   userId VARCHAR(25),
+   PRIMARY KEY(id),
+   FOREIGN KEY(login) 
+	REFERENCES Usuario(login),
+   FOREIGN KEY(tipo) 
+        REFERENCES PROBLEMA_TIPO(id)
+);
